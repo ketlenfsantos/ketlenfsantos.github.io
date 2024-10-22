33f2640c0cbf963fbd3060f4d623c73e96cd10cf
Ketlen Figueiredo <ketlen_figueiredo@hotmail.com>
2024-10-22 14:27:29 -0300

    add js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function menuShow(){
+  const menuMobile = document.querySelector('.mobile-menu');
+  if(menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');}
+    else {
+      menuMobile.classList.add('open');
+    }
+  }
+
+  
+    new SimpleAnime();
+  
+  
