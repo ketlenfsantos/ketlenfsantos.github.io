77eb53cee9995b7ddb7814b41227205ec812a9b2
Ketlen Figueiredo <ketlen_figueiredo@hotmail.com>
2024-10-22 17:32:18 -0300

    troca

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-function menuShow(){
-  const menuMobile = document.querySelector('.mobile-menu');
-  if(menuMobile.classList.contains('open')) {
-    menuMobile.classList.remove('open');}
-    else {
-      menuMobile.classList.add('open');
-    }
-  }
+// function menuShow(){
+//   const menuMobile = document.querySelector('.mobile-menu');
+//   if(menuMobile.classList.contains('open')) {
+//     menuMobile.classList.remove('open');}
+//     else {
+//       menuMobile.classList.add('open');
+//     }
+//   }
 
   
-    new SimpleAnime();
+//     new SimpleAnime();
   
   
